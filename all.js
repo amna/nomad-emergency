000c47b7d159c36b158f3ae7be8b6fd9449faa4b
amna <amna@users.noreply.github.com>
2015-07-04 23:30:01 -0400

    Update all.js

--- all.js
+++ all.js
@@ -1,8 +1,19 @@
 function initialize() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
+    var myLatlng1 = new google.maps.LatLng(53.65914, 0.072050);
+
+     var mapOptions = {
+         zoom: 10,
+         center: myLatlng1,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+     };
+     var map = new google.maps.Map(document.getElementById('map'),
+     mapOptions);
+
+     if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function (position) {
+             initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+             map.setCenter(initialLocation);
+         });
     };
                     
     // Display a map on the page
